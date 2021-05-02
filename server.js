010f850b76d35f9d991c8fed01135eaedd792a06
Elyxcode <dfjuarezreyes21@gmail.com>
2021-05-02 13:15:31 -0600

    Se agregó index, home y archivos bootstrap css y js

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    // res.send('Hello world');
-    let salida = {
-        nombre: 'Douglas',
-        edad: '29',
-        url: req.url
-    }
+app.use(express.static(__dirname + '/public'));
 
-    res.send(salida);
-});
+// app.get('/', (req, res) => {
+//     // res.send('Hello world');
+//     let salida = {
+//         nombre: 'Douglas',
+//         edad: '29',
+//         url: req.url
+//     }
+
+//     res.send(salida);
+// });
 
 app.listen(3000, () => {
     console.log('Escuchando por el puerto 3000');
 });
+ 
